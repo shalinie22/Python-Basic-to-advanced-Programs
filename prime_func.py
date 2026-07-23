6fa8db06a22c5165d691cd16c56fed1a97e660fb
shalinie <shalushalinie22@gmail.com>
2026-07-24 00:18:23 +0530

    Changes done in prime number

--- prime_func.py
+++ prime_func.py
@@ -3,10 +3,10 @@
 # Write a function is_prime(n) that returns True if the number is prime; otherwise False.
 
 def is_prime(n):
-    if n==1:
-        return True
+    if n<=1:
+        return False
     elif n>1:
-        for i in range(2,n):
+        for i in range(2,int(n**0.5)+1):
             if n%i==0:
                 return False
         else:
